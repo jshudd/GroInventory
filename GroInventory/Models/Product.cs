ca846d9c9f41cb3f4b9e957ed08e3dd7ec912f62
Jeremy Huddleston <jshudd@gmail.com>
2022-08-22 10:41:02 -0500

    Calc & Display Profit/Unit
    calculating profit and displaying on index and viewproduct pages

--- GroInventory/Models/Product.cs
+++ GroInventory/Models/Product.cs
@@ -22,6 +22,13 @@ namespace GroInventory.Models
         public int UnitsPerCase { get; set; }
         public double CurrentInventory { get; set; }
         public IEnumerable<Department> Departments { get; set; }
+        public double CurrentPrice
+        {
+            get
+            {
+                return (this.OnSale) ? this.SalePrice : this.Price;
+            }
+        }
         public string ConvertedPrice
         {
             get
@@ -43,12 +50,34 @@ namespace GroInventory.Models
                 return (this.CaseCost < 1) ? (this.CaseCost * 100) + "¢" : "$" + this.CaseCost.ToString("N");
             }
         }
+        public string ConvertedCurrentPrice
+        {
+            get
+            {
+                return (this.CurrentPrice < 1) ? (this.CurrentPrice * 100) + "¢" : "$" + this.CurrentPrice.ToString("N");
+            }
+        }
+        public string ConvertedProfit
+        {
+            get
+            {
+                return (this.Profit < 1 && this.Profit > 0) ? Math.Round((this.Profit * 100), 2) + "¢" : "$" + Math.Round(this.Profit, 2).ToString("N");
+            }
+        }
 
         public string DeptName
         {
             get
             {
                 return Department.DeptList.ElementAt(DeptID).DeptName;
+            }
+        }
+
+        public double Profit
+        {
+            get
+            {
+                return this.CurrentPrice - this.CaseCost / this.UnitsPerCase;
             }
         }
     }
